3709017bad32098bde2abfe6b50bf136ab466872
JeongWook Lee <jeongwle@JeongWookui-MacBookPro.local>
2021-09-13 19:12:19 +0900

    CPP03 : ex01 구조만 구조만 짜놓았음

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeongwle <jeongwle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/12 21:50:51 by jeongwle          #+#    #+#             */
+/*   Updated: 2021/09/12 21:56:54 by jeongwle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+# include <iostream>
+# include <string>
+
+class ClapTrap{
+private:
+    std::string     _Name;
+    unsigned int    _HitPoints;
+    unsigned int    _EnergyPoints;
+    unsigned int    _AttackDamage;
+public:
+    ClapTrap();
+    ClapTrap(const std::string name);
+    ClapTrap(const ClapTrap& copy);
+    ~ClapTrap();
+    ClapTrap&   operator=(const ClapTrap& trap);
+    void    attack(std::string const& target);
+    void    takeDamage(unsigned int amount);
+    void    beRepaired(unsigned int amount);
+    std::string getName() const;
+    unsigned int    getHitPoints() const;
+    unsigned int    getAttackDamage() const;
+    void    setAttackDamage(unsigned int amount);
+    void    setName(std::string name);
+};
+
+#endif

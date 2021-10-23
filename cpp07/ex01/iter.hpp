4e5d8bda13a087fda0fb87ebad7fe7987691f0ed
JeongWook Lee <jeongwle@JeongWookui-MacBookPro.local>
2021-10-23 14:14:15 +0900

    CPP07 : modify iter 매개변수를 const로 받지 않았던 문제를 인지하고 바꾸었다.

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: jeongwle <jeongwle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 21:04:49 by jeongwle          #+#    #+#             */
-/*   Updated: 2021/10/17 15:59:25 by jeongwle         ###   ########.fr       */
+/*   Updated: 2021/10/23 14:12:55 by jeongwle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <iostream>
 
 template <typename T>
-void iter(T* array, int length, void (*func)(const T&)){
+void iter(const T* array, int length, void (*func)(const T&)){
     for (int i = 0; i < length; i++){
         func(array[i]);
     }
